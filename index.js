c15a7c4bee154e829b945b906855590a1aa0ae7a
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-12 17:50:25 +0200

    testing CI

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ module.exports = async function (context, req) {
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    body: "TEST US, Testing CI NEW NEW NEW",
+    body: "TEST US, Testing CI NEW",
   };
 };
