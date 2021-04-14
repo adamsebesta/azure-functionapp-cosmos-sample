977facff7fbcf2d9bab93d1bb50663a9e6d2fd4d
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-14 14:59:45 +0200

    use azure keyvault token to access cosmos

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,28 +1,45 @@
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
-const { CosmosClient } = require("@azure/cosmos");
+const {
+    DefaultAzureCredential
+} = require("@azure/identity");
+const {
+    SecretClient
+} = require("@azure/keyvault-secrets");
+const {
+    CosmosClient
+} = require("@azure/cosmos");
 
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+    context.log("JavaScript HTTP trigger function processed a request.");
 
-  const credential = new DefaultAzureCredential();
-  const url = process.env["KEYVAULT_URI"] || "<keyvault-url>";
-  const client = new SecretClient(url, credential);
+    const credential = new DefaultAzureCredential();
+    const url = process.env["KEYVAULT_URI"] || "<keyvault-url>";
+    const client = new SecretClient(url, credential);
 
-  const secretName = "cosmos-primary-key";
+    const secretName = "cosmos-primary-key";
 
-  const secret = await client.getSecret(secretName);
-  console.log("secret: ", secret);
+    const secret = await client.getSecret(secretName);
+    console.log("secret: ", secret);
 
-  //   const endpoint = "https://aasha-proto.documents.azure.com/";
-  //   const key = getResult;
-  //   const client = new CosmosClient({
-  //     endpoint,
-  //     key,
-  //   });
+    const endpoint = "https://aasha-proto.documents.azure.com/";
+    const key = secret.value;
+    const client = new CosmosClient({
+        endpoint,
+        key,
+    });
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: secret,
-  };
+    const database = client.database('ToDoList');
+    const container = database.container('Items');
+
+    const querySpec = {
+        query: "SELECT * from c"
+    };
+
+    const items = await container.items
+        .query(querySpec)
+        .fetchAll();
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: items,
+    };
 };
