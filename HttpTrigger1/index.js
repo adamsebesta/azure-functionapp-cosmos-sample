94ba745ee873d9dd97f5547acb6b68cccfd9fbad
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-14 11:22:22 +0200

    testing azure MSI

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,18 @@
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from ("@azure/keyvault-secrets");
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name ?
-        "Hello, " + name + ". This HTTP triggered function executed successfully." :
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const credential = new DefaultAzureCredential();
+  const client = new SecretClient(
+    `https://aasha-proto.documents.azure.com/`,
+    credential
+    );
+    console.log(client)
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: 'TEST CI'
-    };
-}
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: client,
+  };
+};
