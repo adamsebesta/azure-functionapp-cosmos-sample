8aec6c3f175537cade9d8b506b9604344e227042
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-14 14:07:00 +0200

    get cosmos key from keyvault

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,18 +1,28 @@
-const DefaultAzureCredential = require("@azure/identity");
-const SecretClient = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { CosmosClient } = require("@azure/cosmos");
 
 module.exports = async function (context, req) {
-    context.log("JavaScript HTTP trigger function processed a request.");
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    const credential = new DefaultAzureCredential();
-    const client = new SecretClient(
-        `https://aasha-proto.documents.azure.com/`,
-        credential
-    );
-    console.log(client)
+  const credential = new DefaultAzureCredential();
+  const url = process.env["KEYVAULT_URI"] || "<keyvault-url>";
+  const client = new SecretClient(url, credential);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: client,
-    };
+  const secretName = "cosmos-primary-key";
+
+  const secret = await client.getSecret(secretName);
+  console.log("secret: ", secret);
+
+  //   const endpoint = "https://aasha-proto.documents.azure.com/";
+  //   const key = getResult;
+  //   const client = new CosmosClient({
+  //     endpoint,
+  //     key,
+  //   });
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: secret,
+  };
 };
