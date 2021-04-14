43eb72f22f48b4f0e613d1ba445045492d1e1f17
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-14 12:53:40 +0200

    put imports inside import

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,18 +1,21 @@
-import { DefaultAzureCredential } from "@azure/identity";
-import { SecretClient } from ("@azure/keyvault-secrets");
-
 module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
+    import {
+        DefaultAzureCredential
+    } from "@azure/identity";
+    import {
+        SecretClient
+    } from("@azure/keyvault-secrets");
+    context.log("JavaScript HTTP trigger function processed a request.");
 
-  const credential = new DefaultAzureCredential();
-  const client = new SecretClient(
-    `https://aasha-proto.documents.azure.com/`,
-    credential
+    const credential = new DefaultAzureCredential();
+    const client = new SecretClient(
+        `https://aasha-proto.documents.azure.com/`,
+        credential
     );
     console.log(client)
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: client,
-  };
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: client,
+    };
 };
