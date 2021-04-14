3c2174fc8f8f2e634e985eb39e92badcdf4a1831
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-14 18:00:17 +0200

    refactored, testing local dev

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -3,17 +3,20 @@ const Helpers = require("../helpers");
 module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
     const key = Helpers.getSecretFromVault("cosmos-primary-key");
-    const db = Helpers.initCosmos(key, 'ToDoList');
+    const db = await Helpers.initCosmos(key, 'ToDoList');
     const container = db.container('Items');
-
+    // console.log(container.items)
     const querySpec = {
         query: "SELECT * from c"
     };
-
-    const items = await container.items
-        .query(querySpec)
-        .fetchAll();
-
+    try {
+        const items = await container.items
+            .query(querySpec)
+            .fetchAll();
+    } catch (e) {
+        console.log(e)
+    }
+    console.log(items)
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: items,
--- helpers/index.js
+++ helpers/index.js
@@ -11,17 +11,17 @@ const {
 const helpersObject = {
   async getSecretFromVault(name) {
     const credential = new DefaultAzureCredential();
-    const url = process.env["KEYVAULT_URI"] || "<keyvault-url>";
+    const url = process.env["KEYVAULT_URI"]
     const client = new SecretClient(url, credential);
     return await client.getSecret(name);
   },
   async initCosmos(key, db) {
-    const endpoint = process.env["COSMOS_URI"] || "<COSMOS_URI>";
+    const endpoint = process.env["COSMOS_URI"]
     const cosmosClient = new CosmosClient({
       endpoint,
       key,
     });
-    return await cosmosClient.database(db);
+    return cosmosClient.database(db);
 
   }
 
