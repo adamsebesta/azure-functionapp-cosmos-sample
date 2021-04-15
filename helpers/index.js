ca06800faaf497ba971e781289028599eac7d7c0
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-15 11:10:56 +0200

    added .value to cloud key

--- helpers/index.js
+++ helpers/index.js
@@ -13,26 +13,24 @@ const helpersObject = {
     const credential = new DefaultAzureCredential();
     const url = process.env["KEYVAULT_URI"]
     const client = new SecretClient(url, credential);
-    return await client.getSecret(name);
+    return await client.getSecret(name).value;
   },
-  async initCosmos(dbName, contName, context) {
+  async initCosmos(dbName, contName) {
     // get runtime varaible (dev) or db key from helper (prod)  
     let key = process.env['COSMOS_DB_KEY'] || await this.getSecretFromVault("cosmos-primary-key");
-    context.log(key)
     const endpoint = process.env["COSMOS_URI"]
-    context.log(endpoint)
     // init client
-    // const cosmosClient = new CosmosClient({
-    //   endpoint,
-    //   key,
-    // });
-    // // get db / create if doesnt exist
-    // const
-    //   db = await cosmosClient.databases.createIfNotExists({
-    //     id: dbName
-    //   });
-    // const container = db.database.container(contName);
-    // return container;
+    const cosmosClient = new CosmosClient({
+      endpoint,
+      key,
+    });
+    // get db / create if doesnt exist
+    const
+      db = await cosmosClient.databases.createIfNotExists({
+        id: dbName
+      });
+    const container = db.database.container(contName);
+    return container;
   }
 }
 
