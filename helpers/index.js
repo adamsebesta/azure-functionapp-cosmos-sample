7025e113ba84c36ebe85445b263d21880aeff231
Adam Sebesta <adam.sebesta5@gmail.com>
2021-04-15 12:07:01 +0200

    final build for node.js cosmos connector

--- helpers/index.js
+++ helpers/index.js
@@ -14,26 +14,24 @@ const helpersObject = {
     const url = process.env["KEYVAULT_URI"]
     const client = new SecretClient(url, credential);
     let secret = await client.getSecret(name);
-    context.log(secret)
     return secret.value
   },
-  async initCosmos(dbName, contName, context) {
+  async initCosmos(dbName, contName) {
     // get runtime varaible (dev) or db key from helper (prod)  
-    let key = process.env['COSMOS_DB_KEY'] || await this.getSecretFromVault("cosmos-primary-key", context);
+    let key = process.env['COSMOS_DB_KEY'] || await this.getSecretFromVault("cosmos-primary-key");
     const endpoint = process.env["COSMOS_URI"]
     // init client
-    context.log(endpoint, key)
     const cosmosClient = new CosmosClient({
       endpoint,
       key,
     });
     // get db / create if doesnt exist
-    // const
-    //   db = await cosmosClient.databases.createIfNotExists({
-    //     id: dbName
-    //   });
-    // const container = db.database.container(contName);
-    // return container;
+    const
+      db = await cosmosClient.databases.createIfNotExists({
+        id: dbName
+      });
+    const container = db.database.container(contName);
+    return container;
   }
 }
 
